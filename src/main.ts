6f3859c593bda844a3b0d6e9f5e36cfb1fba65e2
Leonardorossato <leonardorossato21@gmail.com>
2022-11-03 17:39:27 -0300

    swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
+import { json } from 'express';
 import { AppModule } from './app.module';
+import PostgresSource from './../orm.config.js'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Spotify Backend Clone in NestJs')
+    .setDescription('Rotas da aplicação do back-end')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+    },
+  });
+  PostgresSource.initialize();
   await app.listen(3000);
 }
 bootstrap();

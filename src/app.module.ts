87c2c517a6c7cd9dded114f000cd15028fd437bf
Leonardo Adami Rossato Rodrigues <34960160+Leonardorossato@users.noreply.github.com>
2022-11-03 15:27:09 -0300

    import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TrainersModule } from './trainers/trainers.module';
 import { PokemonsModule } from './pokemons/pokemons.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TrainersModule, PokemonsModule],
+  imports: [TrainersModule, PokemonsModule, AuthModule],
 })
 export class AppModule {}

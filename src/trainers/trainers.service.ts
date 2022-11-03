af7daeac3b1480d0d46ce1d6e3640d2d76817f33
Leonardorossato <leonardorossato21@gmail.com>
2022-11-03 18:10:14 -0300

    services to trainer ready

--- src/trainers/trainers.service.ts
+++ src/trainers/trainers.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTrainerDTO } from './dto/create.trainer.dto';
+import { Trainer } from './entities/trainer.entity';
 
 @Injectable()
-export class TrainersService {}
+export class TrainersService {
+  constructor(
+    @InjectRepository(Trainer) private trainerRepository: Repository<Trainer>,
+  ) {}
+
+  async getAllTrainers(): Promise<Trainer[]> {
+    try {
+      const trainer = this.trainerRepository.find();
+      return trainer;
+    } catch (error) {
+      throw new HttpException('Erro to find trainer', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  async createTrainer(dto: CreateTrainerDTO) {
+    try {
+      const trainer = this.trainerRepository.create(dto);
+      return trainer;
+    } catch (error) {
+      throw new HttpException('Erro to create trainer', HttpStatus.BAD_REQUEST);
+    }
+  }
+}

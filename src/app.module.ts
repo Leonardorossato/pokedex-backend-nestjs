a87b6a44fc4a74668efd28b9ccb2a6b385a4d7af
Leonardo Adami Rossato Rodrigues <34960160+Leonardorossato@users.noreply.github.com>
2022-11-03 15:35:08 -0300

    import

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { PokemonsModule } from './pokemons/pokemons.module';
 import { AuthModule } from './auth/auth.module';
 import { GymModule } from './gym/gym.module';
 import { GymLidersModule } from './gym-liders/gym-liders.module';
+import { LeagueModule } from './league/league.module';
 
 @Module({
-  imports: [TrainersModule, PokemonsModule, AuthModule, GymModule, GymLidersModule],
+  imports: [TrainersModule, PokemonsModule, AuthModule, GymModule, GymLidersModule, LeagueModule],
 })
 export class AppModule {}

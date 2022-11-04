9edefe50b3a8c1b440c97dba66cf80983ad416ae
Leonardo Adami Rossato Rodrigues <34960160+Leonardorossato@users.noreply.github.com>
2022-11-04 11:06:36 -0300

    controller method auth ready

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-
+import { AuthLoginDTO } from './dto/auth.login.dto';
+import { LoginInterface } from './interface/Ilogin.interface';
+import { RegisterAuthDTO } from './dto/auth.register.dto';
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/login')
+  async login(@Body() dto: AuthLoginDTO): Promise<LoginInterface> {
+    return await this.authService.loginCredentials(dto);
+  }
+
+  @Post('/register')
+  async register(@Body() dto: RegisterAuthDTO): Promise<any> {
+    return await this.authService.registerCredentials(dto);
+  }
 }

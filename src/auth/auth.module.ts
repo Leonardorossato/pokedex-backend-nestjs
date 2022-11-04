ad177361aebb6ab88d3a39ef072b0531a49c82ca
Leonardo Adami Rossato Rodrigues <34960160+Leonardorossato@users.noreply.github.com>
2022-11-04 10:02:09 -0300

    jwt and passport modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-
+import { TrainersModule } from '../trainers/trainers.module';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
+  imports: [
+    TrainersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async () => ({
+        secret: process.env.JWT_SECRET as string,
+        signOptions: { expiresIn: '1d', algorithm: 'HS384' },
+        verifyOptions: {
+          algorithms: ['HS384'],
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })

1e99bd7dba7cebc43e6a125a44a1f69452b2a912
Leonardo Adami Rossato Rodrigues <34960160+Leonardorossato@users.noreply.github.com>
2022-11-04 09:17:36 -0300

    ok

--- src/auth/dto/auth.register.dto.ts
+++ src/auth/dto/auth.register.dto.ts
@@ -1,18 +1,30 @@
 /* eslint-disable prettier/prettier */
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterAuthDTO {
   @IsNotEmpty()
   @ApiProperty({ nullable: false, type: String })
+  @MinLength(3)
+  @MaxLength(200)
   name: string;
 
   @IsEmail({}, { message: 'Email must be valid' })
   @IsString()
+  @MinLength(3)
+  @MaxLength(200)
   @ApiProperty({ nullable: false, type: String })
   email: string;
 
   @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(200)
   @ApiProperty({ nullable: false, type: String })
   password: string;
 }
